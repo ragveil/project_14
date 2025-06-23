f1323066c35cfbea19c33c54484601342ffacb9a
Nadezhda Kornienko <service911@bk.ru>
2025-06-24 00:32:01 +1000

    feat: add new features to existing classes

--- src/classes.py
+++ src/classes.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class Product:
     """
     Класс для представления продукта.
@@ -18,20 +21,54 @@ class Product:
         """
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    def __repr__(self):
+        return f'{self.name}, {self.price} руб., Остаток: {self.quantity} шт.'
+
+    @classmethod
+    def new_product(cls, prod_dict: dict):
+        products = Category.products
+        if prod_dict.keys() == {'name', 'description', 'price', 'quantity'}:
+            name, description, price, quantity = prod_dict.values()
+            for item in products:
+                if item.name == name:
+                    item.quantity += quantity
+                    item.price = max(price, item.price)
+                else:
+                    return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print(f'Указанное значение ниже или равно нулю.')
+        elif new_price < self.price:
+            answer = input('Указанная цена ниже уже установленной. Введите "Y", чтобы подтвердить изменение цены.')
+            if answer.lower() == 'y':
+                self.__price = new_price
+            else:
+                print(f'Цена осталась без изменений - {self.__price}.')
+
+    # def __str__(self):
+    #     return Product.__str__(self)
+    # def __repr__(self):
+    #     return Product.__repr__(self)
 
 
 class Category:
     """
     Класс для представления категории.
     """
-
-    products: list[Product]
     product_count: int = 0
     category_count: int = 0
 
-    def __init__(self, name: str, description: str, products: list[Product]):
+
+    def __init__(self, name: str, description: str, products: Optional[list[Product]] = None):
         """
         Метод для инициализации экземпляра класса категории.
         :param name: Наименование категории, str.
@@ -40,7 +77,24 @@ class Category:
         """
         self.name = name
         self.description = description
-        self.products = products
-        self.total_count = sum(Product.quantity for Product in products)
-        Category.product_count += len(products)
+        self.__products = products
+        self.total_count = sum(product.quantity for product in products)
         Category.category_count += 1
+
+    def add_product(self, product: Product):
+        """
+        Добавляет продукт в категорию.
+        :param product:
+        :return:
+        """
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count = len(self.__products)
+
+    @property
+    def products(self) -> list[Product]:
+        """
+        Выводит список продуктов.
+        :return:
+        """
+        return self.__products
