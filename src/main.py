be7524d2ee61b65a029c879b22b12e4a399b10a1
Nadezhda Kornienko <service911@bk.ru>
2025-07-02 21:54:49 +1000

    refactor: add some minor changes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from src.classes import Category, LawnGrass, Smartphone
+from src.classes import Category, LawnGrass, Smartphone  # pragma: no cover
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     smartphone1 = Smartphone(
         "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый"
     )
